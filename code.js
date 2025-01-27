c5aab83f1cbe199b6566768967f0c84588726e07
livi5599 <obrown1@uwyo.edu>
2025-01-26 19:54:34 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,55 @@
 function flip(array, n) {
-    return array;
+    var tmp = [];
+
+    if (n > array.length) {
+        array.reverse();
+        return array;
+    }
+    else {
+        for (i = 0; i < n; i++) {
+            tmp.push(array[i]);
+        }
+        
+        tmp.reverse();
+
+        for (i = 0; i < n; i++) {
+            array[i] = tmp[i];
+        }
+        return array;
+    }
 }
 
-// Use only flip() here to manipulate the array
 function pancakeSort(array) {
+    var numSorted = 0;
+    while (numSorted != array.length) {
+        var g8El = array[0];
+        for (i = 0; i < (array.length - numSorted); i++) {
+            if (array[i] > g8El) {
+                g8El = array[i];
+            }
+        }
+
+        if (g8El != array[0]) {
+            flip(array, array.indexOf(g8El) + 1);
+            if (numSorted == 0) {
+                flip(array, (array.length + 1));
+                numSorted += 1;
+            }
+            else {
+                flip(array, (array.length - numSorted));
+                numSorted += 1;
+            }
+        }
+        else {
+            if (numSorted == 0) {
+                flip(array, (array.length + 1));
+                numSorted += 1;
+            }
+            else {
+                flip(array, (array.length - numSorted));
+                numSorted += 1;
+            }
+        }
+    }
     return array;
 }
